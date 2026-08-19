24bf6f0ffcccb3e5ce40810d09df6f1edfdbe90e
yiruzu <yiruzu@protonmail.com>
2026-08-19 16:20:15 +0200

    init

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+import postcssSimpleVars from "postcss-simple-vars"
+import postcssNested from "postcss-nested"
+
+export default {
+  plugins: {
+    "postcss-simple-vars": postcssSimpleVars(),
+    "postcss-nested": postcssNested(),
+    "@tailwindcss/postcss": {},
+  },
+}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import "./assets/styles/global.css"
+import "./assets/styles/fonts.css"
+import "./assets/styles/colors.css"
+
+import { createApp } from "vue"
+
+import App from "./App.vue"
+
+import { createPinia } from "pinia"
+import { i18n } from "./locales"
+import router from "./router"
+import LenisVue from "lenis/vue"
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+app.use(i18n)
+app.use(LenisVue)
+
+app.mount("#app")
