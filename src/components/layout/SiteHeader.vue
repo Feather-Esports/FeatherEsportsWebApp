89837705e074f6816f21b3429da0eb83e6ef198a
yiruzu <yiruzu@protonmail.com>
2026-09-08 05:38:04 +0200

    refactor(SiteHeader): remove unused code

--- src/components/layout/SiteHeader.vue
+++ src/components/layout/SiteHeader.vue
@@ -19,7 +19,7 @@ const navigation = [
 
 <template>
   <header class="site-header">
-    <a class="wordmark" href="/" :aria-label="t('footer.homeLabel')"
+    <a class="wordmark" href="/"
       ><Icon icon="game-icons:feather" width="2.125rem" /><span>Feather</span></a
     >
     <button
