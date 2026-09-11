ae4960f9801e1ec1c3ffd28b3ce330bba342835e
yiruzu <yiruzu@protonmail.com>
2026-09-11 22:29:18 +0200

    chore: adjust gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       dts: "components.d.ts",
     }),
   ],
+  base: "/FeatherEsportsWebApp/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
